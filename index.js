c86237abbb266ee870633188e302ed5521e45026
JNokikana <herrapentti@gmail.com>
2020-05-21 02:03:37 +0300

    Works better now.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const MongoDB = require('./implementation/mongodb.js');
 const Memory = require('./implementation/memory.js');
+let Persistence;
 
+if (process.env.PERSISTENCE_PROVIDER === "mongodb") {
+    Persistence = require('./implementation/mongodb.js');
+}
+else if (process.env.PERSISTENCE_PROVIDER === "memory") {
+    Persistence = require('./implementation/memory.js');
+}
 module.exports = {
     MongoDB,
-    Memory
+    Memory,
+    Persistence
 }
